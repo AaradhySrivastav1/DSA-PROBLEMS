257be0ae6b424e060bc7c8248bd747d039f45b6e
Aaradhy Srivastava <aaradhysrivastav2908@gmail.com>
2026-02-17 14:06:56 +0530

    Add files via upload

---
+++ minimum_shop_penalty.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int bestClosingTime(string customers) {
+        int penalty = 0;
+
+        for (char c : customers) {
+            if (c == 'Y') penalty++;
+        }
+
+        int minPenalty = penalty;
+        int bestHour = 0;
+
+        for (int i = 0; i < customers.size(); i++) {
+            if (customers[i] == 'Y')
+                penalty--;
+            else
+                penalty++;
+
+            if (penalty < minPenalty) {
+                minPenalty = penalty;
+                bestHour = i + 1;
+            }
+        }
+
+        return bestHour;
+    }
+};
+
+int main() {   // ✅ REQUIRED
+    Solution s;
+    string customers = "YYNY";
+    cout << s.bestClosingTime(customers);
+    return 0;
+}
